c620c611c2f9a3499ea8108c505bd45963c8b38b
Ashlin <ashlinaronin@gmail.com>
2015-09-11 09:11:10 -0700

    Add initial files

---
+++ spec/specs.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe('format', function() {
+//     it("handles uppercase", function() {
+//         expect(format("SANTACLAUS")).to.equal("santaclaus");
+//     });
+//
+//     it("handles whitespace", function() {
+//         expect(format("SANTA CLAUS")).to.equal("santaclaus");
+//         expect(format("there are lots of spaces")).to.equal("therearelotsofspaces");
+//     });
+//
+//     it("handles special characters", function() {
+//         expect(format("$ANTA CLAUS number 1!!")).to.equal("antaclausnumber1");
+//     });
+// });
