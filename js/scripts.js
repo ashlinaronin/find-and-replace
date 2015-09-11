8d9ee42dd9f1d7b6372aa12c11304abaf7807a2f
Ashlin <ashlinaronin@gmail.com>
2015-09-11 09:41:22 -0700

    Use regex to pass all tests

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
+/* Prepare a regex statement with the word to find and turn on
+** global and ignore case settings. Then run the replace function
+** with that regex and replace any occurences with replacement. */
 var findAndReplace = function(stringToSearch, wordToFind, replacement) {
-    var replacedString = stringToSearch.replace(wordToFind, replacement);
-
-    return replacedString;
+    var regex = new RegExp('(' + wordToFind + ')', 'gi');
+    return stringToSearch.replace(regex, replacement);
 }
